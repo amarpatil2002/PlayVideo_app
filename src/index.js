81e2006c3d328a8bb0b74057f4d9efca7bc66aaf
amarpatil2002 <patilamarjit2002@gmail.com>
2025-08-16 19:14:01 +0530

    custom api response and error handling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
 import connectDB from "./db/connection.js";
+import app from "./app.js"
 
 dotenv.config({path:'./env'})
 
 connectDB().then(() => {
-    console.log(`Connection successful`);
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is runing at port ${process.env.PORT}`);
+    })
 }).catch((err) => {
     console.log(`Connection error ${err}`);
 })
